033f38028ee7512cd3ee0a646dcdbe906d7520db
YazanSufian99 <yazan.sufian@yahoo.com>
2022-02-16 15:12:13 +0200

    finsh the task and add some style

--- js/app2.js
+++ js/app2.js
@@ -41,51 +41,52 @@ let assest={
     // }
 }
 // task 07
-// var employeeResult=[]
-// function Employee(employeeId,fullName,department,level,image){
-//     this.employeeId=employeeId;
-//     this.fullName=fullName;
-//     this.department=department;
-//     this.level= level; 
-//     this.image=image; 
-//     this.salary=0;
-//     employeeResult.push(this);
-// }
+
+var employeeResult=[]
+function Employee(employeeId,fullName,department,level,image){
+    this.employeeId=employeeId;
+    this.fullName=fullName;
+    this.department=department;
+    this.level= level; 
+    this.image=image; 
+    this.salary=0;
+    employeeResult.push(this);
+}
 
 
-function render(){
+
     let GhaziSamer = new Employee(1000,"Ghazi Samer","Administration",
         "senior","0" );
-        GhaziSamer.calcSalary();
+    // GhaziSamer.calcSalary();
     // console.log(GhaziSamer);
     let LanaAli = new Employee(1001,"Lana Ali","Finance",
     "Senior","0" );
-    LanaAli.calcSalary();
+    // LanaAli.calcSalary();
     // console.log(ahmad);
 
     let TamaraAyoub = new Employee(1002,"Tamara Ayoub","Marketing",
     "Senior","0" );
-    TamaraAyoub.calcSalary();
+    // TamaraAyoub.calcSalary();
 
     let SafiWalid = new Employee(1003,"Safi Walid","Administration",
     "Mid-Senior","0" );
-    SafiWalid.calcSalary();
+    // SafiWalid.calcSalary();
 
     let OmarZaid = new Employee(1004,"Omar Zaid","Development",
     "Senior","0" );
-    OmarZaid.calcSalary();
+    // OmarZaid.calcSalary();
 
     let RanaSaleh = new Employee(1005,"Rana Saleh","Development",
     "Junior","0" );
-    RanaSaleh.calcSalary();
+    // RanaSaleh.calcSalary();
 
     let HadiAhmad = new Employee(1006,"Hadi Ahmad","Finance",
     "Mid-Senior","0" );
-    HadiAhmad.calcSalary();
-    console.log(employeeResult);
+    // HadiAhmad.calcSalary();
+    // console.log(employeeResult);
     // document.write(`<h2>${employeeResult.fullName} ${employeeResult.salary}`)
 
-    }
+
 
 
  Employee.prototype.calcSalary =function(){
@@ -123,25 +124,15 @@ function renderSalayr(min,max){
     return Math.floor(Math.random() * (min - max) ) + min;
 }
 
-render();
 
-//  task 08 ----------------------------------------------------------------------
-let form=document.getElementById("userForm");
-form.addEventListener("click",handelSubmit);
-
-function handelSubmit(event){
-    event.preventDefault();
-    let name= document.getElementById("name").value;
-    let img  =document.getElementById("img").value; 
-    var x = document.getElementById("Department").options.length;
-    for (let i = 0; i < x.length; i++) {
-        if (document.getElementById('Department') != null) {
-            str = document.getElementById("Department").value;
-        }
-        
-    }
+Employee.prototype.render=function(){
+    document.write(`<h2 style ="text-align: center; background-color: lightgreen;">${this.fullName} ${this.salary} </h2>`);
+   
+}
+console.log(employeeResult);
+for (let i = 0; i < employeeResult.length; i++) { 
+    employeeResult[i].calcSalary();// to calc salary for each employee 
+    employeeResult[i].render();// to print in main for each employee 
 
     
-    // document.getElementById("mySelect").options.item(0).text;
-    console.log(name,img,str) ;
 }
---
+++ js/app3.js
@@ -0,0 +1,20 @@
+//  task 08 ----------------------------------------------------------------------
+// let form=document.getElementById("userForm");
+// form.addEventListener("click",handelSubmit);
+
+// function handelSubmit(event){
+//     event.preventDefault();
+//     let name= document.getElementById("name").value;
+//     let img  =document.getElementById("img").value; 
+//     var x = document.getElementById("Department").options.length;
+//     for (let i = 0; i < x.length; i++) {
+//         if (document.getElementById('Department') != null) {
+//             str = document.getElementById("Department").value;
+//         }
+        
+//     }
+
+    
+//     // document.getElementById("mySelect").options.item(0).text;
+//     console.log(name,img,str) ;
+// }
